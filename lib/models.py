6254507f86b82dd058a9228c078c482b2ffc0818
Duncan Kibet <kibet2704@gmail.com>
2025-05-28 07:43:02 +0300

    Add base Company and Dev models with SQLAlchemy

--- lib/models.py
+++ lib/models.py
@@ -1,12 +1,11 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.ext.associationproxy import association_proxy
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
-
 Base = declarative_base(metadata=metadata)
 
 class Company(Base):
@@ -16,10 +15,32 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', back_populates='company', cascade='all, delete-orphan')
+
+    devs = association_proxy('freebies', 'dev')
+    creator= lambda dev: Freebie(dev=dev)
+
     def __repr__(self):
         return f'<Company {self.name}>'
+    
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(
+            item_name=item_name,
+            value=value,
+            company=self,
+            dev=dev
+        )
+        return freebie
+    
+    @classmethod
+    def oldest_company(cls):
+        return cls.quert.order_by(cls.founding_year).first()
 
-class Dev(Base):
+    
+
+
+
+ class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
